a6033329f566a22aaa4d3d4271a2a560996f62e7
Hasan Özen <hasan.ozen1048@gmail.com>
2021-10-24 20:59:58 +0300

    initial

---
+++ Dragons Realm/Assets/Scripts/Core/CameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO: add namespace areas
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 userOffset;
+    private Vector3 offset;
+    
+    [Range(0.01f, 1.0f)] public float smooth = 0.5f;
+
+    public bool lookAtTarget;
+    public bool autoUpdate;
+
+    void Start()
+    {
+        SetCameraOffset();
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPos = target.position + offset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smooth);
+
+        if (lookAtTarget)
+            transform.LookAt(target);
+    }
+    public void UpdateCameraOffset()
+    {
+        transform.position = userOffset;
+        SetCameraOffset();
+        
+        if (lookAtTarget)
+            transform.LookAt(target);
+    }
+
+    void SetCameraOffset()
+    {
+        offset = transform.position - target.position;
+    }
+}
---
+++ Dragons Realm/Assets/Scripts/Editor/CameraFollowEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CameraFollow))]
+public class CameraFollowEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        CameraFollow cameraFollow = (CameraFollow) target;
+
+        if (DrawDefaultInspector())
+        {
+            if (cameraFollow.autoUpdate)
+            {
+                cameraFollow.UpdateCameraOffset();
+            }
+        }
+        
+        if (GUILayout.Button("Update"))
+        {
+            cameraFollow.UpdateCameraOffset();
+        }
+    }
+}
